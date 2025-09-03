0ff9469dc06e72d361c8d533412aad1a5b1fa12b
Andrés <128663050+andr3sr0driguez@users.noreply.github.com>
2025-09-03 14:53:34 -0500

    Resstructure el diseño del login y Añadir soporte para Material Icons y mejorar el formulario de inicio de sesión

--- src/app/view/pages/auth/login/login.component.ts
+++ src/app/view/pages/auth/login/login.component.ts
@@ -8,6 +8,9 @@ import { Session } from '../services/session';
 import { LoginFormModel } from '../models/login-form';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -16,40 +19,49 @@ import { MatInputModule } from '@angular/material/input';
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
+    MatCheckboxModule,
+    MatIconModule,
+    CommonModule
   ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  hide = true;
+  submitted = false;
+
   public loginForm: FormGroup = new LoginFormModel().formLogin();
   public isChecked: boolean = false;
   private cryptoService$ = new CryptoService();
+
   constructor(private loginSession$: Session, private router$: Router) {}
 
   sessionLogin(): void {
-    if (!this.loginForm.invalid) {
-      const data = {
-        Name: this.loginForm.get('username')?.value,
-        Password: '',
-      };
-
-      NormaliceLowerValidators.normaliceData(data);
-
-      data.Password = this.cryptoService$
-        .Encript(this.loginForm.get('password')?.value)
-        .toString();
-
-      this.loginSession$.sessionLogin(data).subscribe(
-        () => {
-          this.router$.navigate(['/home']);
-        },
-        (loginError: Error) => {
-          Toast.fire({
-            icon: 'error',
-            title: 'Usuario o contraseña incorrecta',
-          });
-        }
-      );
+    this.submitted = true; 
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
     }
+
+    const data = {
+      Name: this.loginForm.get('username')?.value,
+      Password: '',
+    };
+
+    NormaliceLowerValidators.normaliceData(data);
+
+    data.Password = this.cryptoService$
+      .Encript(this.loginForm.get('password')?.value)
+      .toString();
+
+    this.loginSession$.sessionLogin(data).subscribe(
+      () => this.router$.navigate(['/home']),
+      () =>
+        Toast.fire({
+          icon: 'error',
+          title: 'Usuario o contraseña incorrecta',
+        })
+    );
   }
 }
+
