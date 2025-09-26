90836255d6769e1912b9b74174651c81ef5a55e9
ren-wmaza <wmaza@rengroup.co>
2025-09-25 23:22:31 -0500

    feat: remove unused SVG assets and update menu component for logout confirmation
    - Deleted unused SVG files: coreui-base-white.svg, coreui-base.svg, coreui-signet-white.svg, coreui-signet.svg.
    - Enhanced menu component to show a logout confirmation modal.
    - Updated styles for menu and profile button.
    - Refactored nav component to utilize the new menu component.
    - Improved user interface in sidenav and dashboard views.

--- src/app/layout/components/menu/menu.ts
+++ src/app/layout/components/menu/menu.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../core/services/auth';
+import { LogoutModal } from '../../../shared/components/logout-modal/logout-modal';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [MatMenuModule, MatDividerModule, MatIconModule, MatButtonModule],
+  imports: [
+    MatMenuModule,
+    MatDividerModule,
+    MatIconModule,
+    MatButtonModule,
+    LogoutModal,
+  ],
   templateUrl: './menu.html',
   styleUrl: './menu.scss',
 })
@@ -18,11 +25,25 @@ export class Menu {
     name: 'Wilmer Maza',
     avatar: 'images/default.png', // pon tu imagen
   };
-
+  readonly showLogoutModal: WritableSignal<boolean> = signal(false);
   constructor(private router: Router, private authService: AuthService) {}
 
   public logout(): void {
     this.authService.clearSession();
     this.router.navigate(['/login']);
+  }
+
+  public showLogoutConfirmation(): void {
+    this.showLogoutModal.set(true);
+  }
+
+  public confirmLogout(): void {
+    this.showLogoutModal.set(false);
+    this.authService.clearSession();
+    this.router.navigate(['/login']);
+  }
+
+  public cancelLogout(): void {
+    this.showLogoutModal.set(false);
   }
 }
